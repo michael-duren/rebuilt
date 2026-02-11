eb662b6e718b81382c8132a14b150a2a1d6a8fef
Michael Duren <michaeld@michaelduren.com>
2026-02-10 18:12:30 -0600

    feat:super basic lb

--- cmd/lb/main.go
+++ cmd/lb/main.go
@@ -3,17 +3,34 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
 var port string
+var index int = 0
+
+const CONFIG_PATH = "./servers.yaml"
 
 func main() {
 	flag.StringVar(&port, "port", ":80", "the port for the load balancer to run on")
 	flag.Parse()
 
-	http.HandleFunc("GET /", requestHandler)
+	servers, err := readYAMLConfig()
+	if err != nil {
+		fmt.Println("error parsing config: \n", err)
+		os.Exit(1)
+	}
+
+	handler, err := requestHandlerFactory(servers)
+	if err != nil {
+		fmt.Println("error creating handler: \n", err)
+		os.Exit(1)
+	}
+	http.HandleFunc("/", handler)
 
 	fmt.Fprintf(os.Stdout, "\nStarting server at %s", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
@@ -22,9 +39,63 @@ func main() {
 	}
 }
 
-func requestHandler(w http.ResponseWriter, r *http.Request) {
-	logRequest(r)
-	w.WriteHeader(http.StatusOK)
+func readYAMLConfig() ([]string, error) {
+	file, err := os.Open(CONFIG_PATH)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "error opening file: %s\n", err)
+		return nil, err
+	}
+
+	b, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "error reading file: %s\n", err)
+		return nil, err
+	}
+	var s = struct {
+		Servers []string `yaml:"servers"`
+	}{}
+
+	err = yaml.Unmarshal(b, &s)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "error unmarshalling config: %s\n", err)
+		return nil, err
+	}
+
+	return s.Servers, nil
+}
+
+type RequestHandler func(http.ResponseWriter, *http.Request)
+
+func requestHandlerFactory(servers []string) (RequestHandler, error) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logRequest(r)
+		res, err := forwardTraffic(servers[index], r)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "error while forwarding traffic: %v\n", err)
+			w.WriteHeader(http.StatusBadGateway)
+			return
+		}
+
+		defer res.Body.Close()
+
+		w.WriteHeader(res.StatusCode)
+		io.Copy(w, res.Body)
+
+		index++
+		index %= len(servers) // reset to zero at end
+	}, nil
+}
+
+func forwardTraffic(path string, r *http.Request) (*http.Response, error) {
+	fullPath := fmt.Sprintf("%s%s", path, r.URL.Path)
+	fmt.Fprintf(os.Stdout, "path: %s, fullPath: %s, r.URL.Path: %s\n", path, fullPath, r.URL.Path)
+	res, err := http.Get(fullPath)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "error executing forward to: %s: %v\n", fullPath, err)
+		return nil, err
+	}
+
+	return res, nil
 }
 
 func logRequest(r *http.Request) {
